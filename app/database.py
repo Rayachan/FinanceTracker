4ef83a7401ae5bbf01799b4123db7b057b77383c
Rayachan <ray_chandra@tamu.edu>
2026-02-25 19:33:20 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,24 +1,22 @@
-import sqlite3 
-import os
-#using sqlite since it works within a container as long as it isnt reloaded
+import sqlite3 #the easiest way to impliment the database part
 
-DATABASE= "expenses.db"
+DATABASE = "expenses.db"
 
 def get_connection():
     conn = sqlite3.connect(DATABASE)
-    conn.row_factory= sqlite3.Row #helps return rows as dicts
+    conn.row_factory = sqlite3.Row
     return conn
 
 def init_db():
     conn = get_connection()
     cursor = conn.cursor()
     cursor.execute('''
-        CRE TABLE IF NOT EXISTS expenses(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        amount REAL NOT NULL,
-        category TEXT NOT NULL,,
-        descrption TEXT,
-        date TEXT NOT NULL
+        CREATE TABLE IF NOT EXISTS expenses (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            amount REAL NOT NULL,
+            category TEXT NOT NULL,
+            description TEXT,
+            date TEXT NOT NULL
         )
     ''')
     conn.commit()
